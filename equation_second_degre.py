d4c85b5d5620f6be5e18b3570d006575ee634e1e
Menelik <meneliknouvellon@gmail.com>
2018-09-18 18:12:15 +0700

    i

--- equation_second_degre.py
+++ equation_second_degre.py
@@ -10,6 +10,26 @@ delta = b*b-4*a*c
 def f(x):
    return a*(x**2) + b*x + c
 
+# ces deux fonctions servent juste à simplifier une fraction si necessaire 
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def simplify_fraction(numer, denom):
+
+    if denom == 0:
+        return "Division by 0 - result undefined"
+    common_divisor = gcd(numer, denom)
+    (reduced_num, reduced_den) = (numer / common_divisor, denom / common_divisor)
+    if reduced_den == 1:
+        return reduced_num
+    elif common_divisor == 1:
+        return numer, denom
+    else:
+        return reduced_num, reduced_den
+
+
 alpha = (-b)/(2*a)
 beta = f(alpha)
 
@@ -17,11 +37,27 @@ x = np.arange(-1000,1000,0.001)
 y=f(x)
 
 print()
-print("Alpha = " + str(alpha))
-print("Beta = " + str(beta))
 
-print("Delta = " + str(delta)) 
+# print alpha et beta
+if alpha == int(alpha):
+  print("Alpha = " + str(int(alpha)))
+else: 
+  wesh2 = simplify_fraction(-b,2*a)
+  print("Alpha = " + str(int(wesh2[0]))+"/"+str(int(wesh2[1]))+" ou sinon " + str(alpha))
 
+if beta == int(beta):
+  print("Alpha = " + str(int(beta)))
+else:
+  wesh2 = simplify_fraction(-((b**2)-(4*a*c)),4*a)
+  print("Beta = " + str(int(wesh2[0]))+"/"+str(int(wesh2[1]))+" ou sinon " + str(beta))
+
+#print delta
+if delta == int(delta):
+  print("Delta = " + str(int(delta)))
+else :
+  print("Delta = " + str(delta)) 
+
+#print les solutions de l'équation + le tableau de signes
 if delta < 0 :
   print("Il n'y a pas de solution pour cette équation")
   plt.xlim(alpha-5.1,alpha+5.1)
@@ -50,13 +86,13 @@ elif delta == 0:
       print()
       print("Tableau de signe de f(x) : ")
       print("          x     | -infini     %s     +infini |" % solution)
-      print("          f(x)  |      +        " + "0" + "      +      " + (len(str(solution))-3)*" " + "|")
+      print("          f(x)  |      +        " + "0" + "      +      " + (len(str(solution))-6)*" " + "|")
       print()
     elif a < 0:
       print()
       print("Tableau de signe de f(x) : ")
       print("          x     | -infini     %s     +infini |" % solution)
-      print("          f(x)  |      -        " + "0" + "      -      " + (len(str(solution))-3)*" " + "|")
+      print("          f(x)  |      -        " + "0" + "      -      " + (len(str(solution))-6)*" " + "|")
       print()
 elif delta > 0:
      solution2 = round((-b-(delta**0.5))/(2*a),2)
@@ -80,7 +116,23 @@ elif delta > 0:
      plt.plot((-b+(delta**0.5))/(2*a),0,'ro')
      plt.text(solution2,-0.4, str(solution2), fontsize=12)
      plt.text(solution1,-0.4, str(solution1), fontsize=12)
-     print("Les solutions de cette équation sont %s et %s" % (solution1,solution2))
+     if solution1 == int(solution1):
+       if solution2 == int(solution2):
+         print("Les solutions de cette équation sont %s et %s" % (int(solution1),int(solution2)))
+       else :
+         mene = simplify_fraction(-b-(delta**0.5),2*a)
+         print("Les solutions de cette équation sont %s et %s/%s qui équivaut à environ %s" % (int(solution1),int(mene[0]),int(mene[1]),str(solution2)))
+     elif solution2 == int(solution2):
+       if solution1 == int(solution1):
+         print("Les solutions de cette équation sont %s et %s" % (int(solution1),int(solution2)))
+       else :
+         mene = simplify_fraction(-b+(delta**0.5),2*a)
+         print("Les solutions de cette équation sont %s et %s/%s qui équivaut à environ %s" % (int(solution2),int(mene[0]),int(mene[1]),str(solution1)))
+     else :
+         mene2 = simplify_fraction(-b+(delta**0.5),2*a)
+         mene = simplify_fraction(-b-(delta**0.5),2*a)
+         print("Les solutions de cette équation sont %s/%s qui équivaut à environ %s et %s/%s qui équivaut à environ %s" % (int(mene[0]),int(mene[1]),str(solution2),int(mene2[0]),int(mene2[1]),str(solution1)))
+
      
      
 
@@ -114,4 +166,4 @@ if courbe == "y":
   figManager = plt.get_current_fig_manager() 
   figManager.full_screen_toggle() 
   plt.show()
-
+  plt.savefig('ok_mec.png')
